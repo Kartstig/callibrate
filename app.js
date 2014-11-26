0e2950b3b8d5ac4ea4deba4c4811c9e5c540ca41
HERMAN SINGH <herman.p.singh@jpmchase.com>
2014-11-26 12:59:02 -0500

    Remove socket.io

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 var config = require('./config');
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/templates/index.html');
-});
-
-io.on('connection', function(socket) {
-  console.log("Connection Established");
 });
 
 http.listen(config.port, function () {
